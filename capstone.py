be72acbf5b181ac39fcd9c369f859670ddf2694f
20elno41 <47048567+20elno41@users.noreply.github.com>
2019-04-04 15:30:56 +0900

    Mouse Motion Binding

--- capstone.py
+++ capstone.py
@@ -5,11 +5,24 @@
 
 # Import SPGL
 import spgl
+import math
 
 # Create Classes
 class Player(spgl.Sprite):
     def __init__(self, shape, color, x, y):
         spgl.Sprite.__init__(self, shape, color, x, y)
+        self.shapesize(stretch_wid=1, stretch_len=3, outline=None)
+        
+    def motion(self, event):
+    	x1 = self.xcor()
+    	y1 = self.ycor()
+    	
+    	x2, y2 = event.x, event.y
+    	x2 -= 400
+    	y2 -= 300
+    	
+    	angle = math.atan2(y2 - y1, x2 - x1) * -180 / math.pi;
+    	self.setheading(angle)
         
 class Goal(spgl.Sprite):
     def __init__(self, shape, color, x, y):
@@ -18,23 +31,24 @@ class Goal(spgl.Sprite):
 class Ball(spgl.Sprite):
     def __init__(self, shape, color, x, y):
         spgl.Sprite.__init__(self, shape, color, x, y)
-
+        
 # Create Functions
 
 # Initial Game setup
 game = spgl.Game(800, 600, "black", "Rainbow Pop by Elno", 0)
 
 # Create Sprites
-player = Player("triangle", "green", 0, 0)
+player = Player("arrow", "green", 0, 0)
 goal = Goal("square", "red", 50, -100)
 ball = Ball("circle", "white", -387, 50)
 
-# Create Labels
-
-# Create Buttons
-
 # Set Keyboard Bindings
+
+# Set Mouse Motion Bindings
+canvas = spgl.turtle.getcanvas()
+canvas.bind('<Motion>', player.motion)
 
 while True:
     # Call the game tick method
     game.tick()
+    
